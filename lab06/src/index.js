c709ef7475fd81327ba328b62e1077dcab911bd1
Jakub Klimczewski <kukli8@o2.pl>
2022-04-11 12:43:28 +0200

    zad1 i 2

---
+++ lab06/src/index.js
@@ -0,0 +1,14 @@
+const runFunc = () => console.log("Completed");
+
+const runFunc2 = () => console.log("Running");
+
+const status2 = "Completed";
+
+switch(status2) {
+    case 'Completed':
+        runFunc();
+        break;
+ 
+    case 'Running': 
+        runFunc2();
+}
